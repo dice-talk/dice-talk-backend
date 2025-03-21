8a6759d04436efa313dfa96d1ce08b0711ba30ba
NamTaekHo <taekho1225@gmail.com>
2025-03-21 17:27:23 +0900

    Item Dto, Service 기능 구현

--- src/main/java/com/example/dice_talk/item/entity/Item.java
+++ src/main/java/com/example/dice_talk/item/entity/Item.java
@@ -4,6 +4,7 @@ import com.example.dice_talk.audit.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -11,6 +12,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
 public class Item extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
