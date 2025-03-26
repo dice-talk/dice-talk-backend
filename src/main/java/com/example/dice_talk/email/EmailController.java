1ea4a77eafeb9d1e37107aa93369a1d7e9b01fff
minggiK <minggiK>
2025-03-26 14:18:52 +0900

    email, toss 코드 수정

--- src/main/java/com/example/dice_talk/email/EmailController.java
+++ src/main/java/com/example/dice_talk/email/EmailController.java
@@ -1,5 +1,6 @@
 package com.example.dice_talk.email;
 
+import com.example.dice_talk.dto.SingleResponseDto;
 import com.example.dice_talk.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,14 +24,17 @@ public class EmailController {
 
     //이메일로 인증번호 전송
     @PostMapping("/email")
-    public ResponseEntity<String> sendVerificationCode(@RequestBody VerificationRequest request) {
+    public ResponseEntity sendVerificationCode(@RequestBody VerificationRequest request) {
         emailService.sendVerificationCode(request.getEmail());
-        return ResponseEntity.ok("인증번호가 이메일로 전송되었습니다.");
+        return ResponseEntity.ok(new SingleResponseDto<>("인증번호가 이메일로 전송되었습니다."));
     }
 
     //입력한 인증번호 검증
     @PostMapping("/verify-code")
-    public ResponseEntity<String> verifyCode(@RequestBody VerificationRequest request) {
+    public ResponseEntity verifyCode(@RequestBody VerificationRequest request) {
+
+        // 테스트용 인증번호 바이패스 코드(임시)
+        if(request.getCode().equals("111111")) return ResponseEntity.ok(new SingleResponseDto<>("이메일 인증이 완료되었습니다."));
         //이메일 중복 확인
         memberService.verifyExistsEmail(request.getEmail());
         boolean isValid = emailService.verifyCode(request.getEmail(), request.getCode());
@@ -39,7 +43,9 @@ public class EmailController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("인증번호가 올바르지 않거나 만료되었습니다.");
         }
 
-        return ResponseEntity.ok("이메일 인증이 완료되었습니다.");
+        return ResponseEntity.ok(new SingleResponseDto<>("이메일 인증이 완료되었습니다."));
     }
+
+
 
 }
