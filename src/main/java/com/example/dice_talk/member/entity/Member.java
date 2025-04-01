416c31a9ede4bced88bd6cdffb58551ddf372946
minggiK <minggiK>
2025-04-01 15:32:51 +0900

    MySQL 설정

--- src/main/java/com/example/dice_talk/member/entity/Member.java
+++ src/main/java/com/example/dice_talk/member/entity/Member.java
@@ -23,7 +23,7 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false, length = 50)
     private String email;
 
     @Column(length = 13, nullable = false, unique = true)
