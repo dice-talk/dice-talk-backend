a499aa1b1df7e2daa4d167cceec6a7c09b74e809
minggiK <minggiK>
2025-05-18 17:26:49 +0900

    프로시 헤더 설정
    # Conflicts:
    #	src/main/resources/application.yml

--- src/main/java/com/example/dice_talk/config/WebConfig.java
+++ src/main/java/com/example/dice_talk/config/WebConfig.java
@@ -1,4 +1,24 @@
 package com.example.dice_talk.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  //모든 경로에 대해
+                        .allowedOrigins("https://dicetalk.co.kr") //하용할 출처
+                        .allowedMethods("GET", "POST", "PATCH", "DELETE")  //허용 HTTP 메서드
+                        .allowedHeaders("*")  //모든 헤더 허용
+                        .allowCredentials(true);   //쿠키 포함 허용 (JWT 등 인증 시 필수)
+            }
+        };
+    }
 }
