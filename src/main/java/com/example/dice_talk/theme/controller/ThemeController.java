eb187ef96728af8872cd57ea874c19764b21b4b5
NamTaekHo <taekho1225@gmail.com>
2025-05-24 11:20:36 +0900

    Theme Swagger 수정

--- src/main/java/com/example/dice_talk/theme/controller/ThemeController.java
+++ src/main/java/com/example/dice_talk/theme/controller/ThemeController.java
@@ -5,6 +5,8 @@ import com.example.dice_talk.dto.MultiResponseDto;
 import com.example.dice_talk.dto.SingleResponseDto;
 import com.example.dice_talk.item.dto.ItemDto;
 import com.example.dice_talk.item.entity.Item;
+import com.example.dice_talk.report.dto.ReportDto;
+import com.example.dice_talk.response.SwaggerErrorResponse;
 import com.example.dice_talk.theme.dto.ThemeDto;
 import com.example.dice_talk.theme.entity.Theme;
 import com.example.dice_talk.theme.mapper.ThemeMapper;
@@ -14,8 +16,12 @@ import com.example.dice_talk.utils.JsonParserUtil;
 import com.example.dice_talk.utils.UriCreator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -33,6 +39,7 @@ import java.net.URI;
 import java.util.List;
 
 @Tag(name = "Theme", description = "테마 API")
+@SecurityRequirement(name = "JWT")
 @RestController
 @RequestMapping("/themes")
 @Validated
@@ -49,18 +56,22 @@ public class ThemeController {
         this.jsonParserUtil = jsonParserUtil;
     }
 
-    @Operation(summary = "테마 생성", description = "새로운 테마를 생성합니다. Multipart/Form-Data로 DTO와 이미지를 전달합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "테마 생성 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
-            @ApiResponse(responseCode = "401", description = "권한 없음"),
-            @ApiResponse(responseCode = "500", description = "서버 오류")
+    @Operation(summary = "테마 등록", description = "새로운 테마를 등록합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "등록 성공"),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자 접근",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"UNAUTHORIZED\", \"message\": \"Authentication is required to access this resource.\"}"))),
+
+            @ApiResponse(responseCode = "403", description = "조회 권한 없음",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"FORBIDDEN\", \"message\": \"Access not allowed\"}"))),
     })
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> postTheme(@Parameter(description = "테마 생성 DTO 문자열(JSON)")
-                                    @Valid @RequestParam String themePostDtoString,
-                                    @Parameter(description = "테마 이미지 파일", required = false)
-                                    @RequestPart(value = "image", required = false) MultipartFile imageFile) throws IOException {
+                                          @Valid @RequestParam("themePostDto") String themePostDtoString,
+                                          @Parameter(description = "테마 이미지 파일", required = false)
+                                          @RequestPart(value = "image", required = false) MultipartFile imageFile) throws IOException {
         AuthorizationUtils.isAdmin();
         ThemeDto.Post postDto = jsonParserUtil.parse(themePostDtoString, ThemeDto.Post.class);
         Theme createdTheme = themeService.createTheme(mapper.themePostToTheme(postDto), imageFile);
@@ -68,20 +79,27 @@ public class ThemeController {
         return ResponseEntity.created(location).build();
     }
 
-    @Operation(summary = "테마 수정", description = "기존 테마를 수정합니다. Multipart/Form-Data로 DTO와 이미지를 전달합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "테마 수정 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
-            @ApiResponse(responseCode = "401", description = "권한 없음"),
-            @ApiResponse(responseCode = "404", description = "테마를 찾을 수 없음"),
-            @ApiResponse(responseCode = "500", description = "서버 오류")
+    @Operation(summary = "테마 수정", description = "기존에 등록된 테마를 수정합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "수정 성공", content = @Content(schema = @Schema(implementation = ThemeDto.Response.class))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자 접근",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"UNAUTHORIZED\", \"message\": \"Authentication is required to access this resource.\"}"))),
+
+            @ApiResponse(responseCode = "403", description = "등록 권한 없음",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"FORBIDDEN\", \"message\": \"Access not allowed\"}"))),
+
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 테마 요청시",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"NOT_FOUND\", \"message\": \"The requested resource could not be found.\"}")))
     })
     @PatchMapping(value = "/{theme-id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<SingleResponseDto<ThemeDto.Response>> patchTheme(
-            @Parameter(description = "테마 식별자", example = "1")
+            @Parameter(description = "테마 ID", example = "1")
             @PathVariable("theme-id") @Positive long themeId,
             @Parameter(description = "테마 수정 DTO 문자열(JSON)")
-            @Valid @RequestParam String themePatchDtoString,
+            @Valid @RequestParam("themePatchDto") String themePatchDtoString,
             @Parameter(description = "테마 이미지 파일", required = false)
             @RequestPart(value = "image", required = false) MultipartFile imageFile
     ) throws IOException {
@@ -92,11 +110,16 @@ public class ThemeController {
     }
 
     // 활성화 상태의 테마 목록 조회 (회원용)
-    @Operation(summary = "운영중인 테마 목록 조회", description = "운영중인 테마 목록을 반환합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "조회 성공"),
-            @ApiResponse(responseCode = "401", description = "권한 없음"),
-            @ApiResponse(responseCode = "500", description = "서버 오류")
+    @Operation(summary = "운영중인 테마 목록 조회", description = "현재 운영중인 테마의 목록을 조회 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = ReportDto.Response.class))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자 접근",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"UNAUTHORIZED\", \"message\": \"Authentication is required to access this resource.\"}"))),
+
+            @ApiResponse(responseCode = "403", description = "조회 권한 없음",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"FORBIDDEN\", \"message\": \"Access not allowed\"}"))),
     })
     @GetMapping
     public ResponseEntity<ListResponseDto<ThemeDto.Response>> getThemes() {
@@ -105,46 +128,68 @@ public class ThemeController {
     }
 
     // 비활성화 상태의 테마까지 조회 (관리자용)
-    @Operation(summary = "전체 테마 목록 조회(관리자)", description = "페이지 및 사이즈로 모든 테마(활성/비활성) 목록을 반환합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "조회 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 파라미터"),
-            @ApiResponse(responseCode = "401", description = "권한 없음"),
-            @ApiResponse(responseCode = "500", description = "서버 오류")
+    @Operation(summary = "관리자용 전체 테마 목록 조회", description = "전체 테마 목록을 조회합니다.(비활성화 포함)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = ThemeDto.Response.class))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자 접근",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"UNAUTHORIZED\", \"message\": \"Authentication is required to access this resource.\"}"))),
+
+            @ApiResponse(responseCode = "403", description = "조회 권한 없음",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"FORBIDDEN\", \"message\": \"Access not allowed\"}"))),
     })
     @GetMapping("/admin")
     public ResponseEntity<MultiResponseDto<ThemeDto.Response>> getAllThemes(@Parameter(description = "페이지 번호(1 이상)", example = "1")
-                                       @Positive @RequestParam int page,
-                                       @Parameter(description = "페이지 크기(1 이상)", example = "10")
-                                       @Positive @RequestParam int size) {
+                                                                            @Positive @RequestParam int page,
+                                                                            @Parameter(description = "페이지 크기(1 이상)", example = "10")
+                                                                            @Positive @RequestParam int size) {
         Page<Theme> themePage = themeService.findThemes(page, size);
         List<Theme> themes = themePage.getContent();
         return new ResponseEntity<>(new MultiResponseDto<>(mapper.themesToThemeResponses(themes), themePage), HttpStatus.OK);
     }
 
-    @Operation(summary = "테마 단일 조회", description = "테마 식별자로 단일 테마를 조회합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "조회 성공"),
-            @ApiResponse(responseCode = "404", description = "테마를 찾을 수 없음"),
-            @ApiResponse(responseCode = "500", description = "서버 오류")
+    @Operation(summary = "테마 상세 조회", description = "특정 테마를 상세조회 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = ThemeDto.Response.class))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자 접근",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"UNAUTHORIZED\", \"message\": \"Authentication is required to access this resource.\"}"))),
+
+            @ApiResponse(responseCode = "403", description = "조회 권한 없음",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"FORBIDDEN\", \"message\": \"Access not allowed\"}"))),
+
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 테마 요청시",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"NOT_FOUND\", \"message\": \"The requested resource could not be found.\"}")))
     })
     @GetMapping("/{theme-id}")
     public ResponseEntity<SingleResponseDto<ThemeDto.Response>> getTheme(
-            @Parameter(description = "테마 식별자", example = "1")
+            @Parameter(description = "테마 ID", example = "1")
             @PathVariable("theme-id") @Positive long themeId) {
         Theme theme = themeService.findTheme(themeId);
         return new ResponseEntity<>(new SingleResponseDto<>(mapper.themeToThemeResponse(theme)), HttpStatus.OK);
     }
 
-    @Operation(summary = "테마 삭제", description = "테마를 삭제합니다.")
-    @ApiResponses({
+    @Operation(summary = "테마 삭제", description = "특정 테마를 삭제합니다.")
+    @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "삭제 성공"),
-            @ApiResponse(responseCode = "404", description = "테마를 찾을 수 없음"),
-            @ApiResponse(responseCode = "500", description = "서버 오류")
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자 접근",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"UNAUTHORIZED\", \"message\": \"Authentication is required to access this resource.\"}"))),
+
+            @ApiResponse(responseCode = "403", description = "삭제 권한 없음",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"FORBIDDEN\", \"message\": \"Access not allowed\"}"))),
+
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 테마 요청시",
+                    content = @Content(schema = @Schema(implementation = SwaggerErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"error\": \"NOT_FOUND\", \"message\": \"The requested resource could not be found.\"}")))
     })
     @DeleteMapping("/{theme-id}")
     public ResponseEntity<Void> deleteTheme(@Parameter(description = "테마 식별자", example = "1")
-                                      @PathVariable("theme-id") long themeId) {
+                                            @PathVariable("theme-id") long themeId) {
         themeService.deleteTheme(themeId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
