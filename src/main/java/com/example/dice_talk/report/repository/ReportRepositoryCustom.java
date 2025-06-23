076251e223f1ffeb9d0aeae01c5a591131f56afb
minggiK <minggiK>
2025-06-23 19:11:37 +0900

    웹페이지 신고 조회 로직 추가

--- src/main/java/com/example/dice_talk/report/repository/ReportRepositoryCustom.java
+++ src/main/java/com/example/dice_talk/report/repository/ReportRepositoryCustom.java
@@ -1,10 +1,12 @@
 package com.example.dice_talk.report.repository;
 
+import com.example.dice_talk.dashboard.dto.DailyCountDto;
 import com.example.dice_talk.report.dto.ReportDto;
 import com.example.dice_talk.report.entity.Report;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReportRepositoryCustom {
@@ -22,4 +24,9 @@ public interface ReportRepositoryCustom {
 
     // 특정 상태의 신고 목록 조회 (선택적)
     Page<ReportDto.Response> findByStatusWithMemberEmail(Report.ReportStatus status, Pageable pageable);
+
+    //웹페이지 : 금일 신고 건수 조회
+    int countReports(LocalDateTime start, LocalDateTime end);
+    //웹페이지 : 주간 신고 건수 조회
+    List<DailyCountDto> countReportsByDate(LocalDateTime start, LocalDateTime end);
 }
