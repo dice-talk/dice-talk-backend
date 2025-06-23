4ba6af007d495699b8fdc2a7656bb802f383c1f8
minggiK <minggiK>
2025-06-23 18:33:59 +0900

    웹페이지 조회 로직 구현

--- src/main/java/com/example/dice_talk/dicelog/repository/DiceLogRepository.java
+++ src/main/java/com/example/dice_talk/dicelog/repository/DiceLogRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface DiceLogRepository extends JpaRepository<DiceLog, Long> {
     Page<DiceLog> findAllByMember_MemberId(long memberId, Pageable pageable);
+    //관리자 대시보드 (사용자 item 결제 내역)
+    int countByCreatedAtBetweenAndType(LocalDateTime start, LocalDateTime end, DiceLog.LogType type);
 }
