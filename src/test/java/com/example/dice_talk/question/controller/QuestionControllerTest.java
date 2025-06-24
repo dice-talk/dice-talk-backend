4e002af25126c6bf3ceec805080751b8e466d96f
minggiK <minggiK>
2025-06-24 13:26:58 +0900

    타입 명시

--- src/test/java/com/example/dice_talk/question/controller/QuestionControllerTest.java
+++ src/test/java/com/example/dice_talk/question/controller/QuestionControllerTest.java
@@ -1,40 +1,40 @@
-package com.example.dice_talk.question.controller;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class QuestionControllerTest {
-
-    @Test
-    void postQuestion() {
-    }
-
-    @Test
-    void patchQuestion() {
-    }
-
-    @Test
-    void getQuestion() {
-    }
-
-    @Test
-    void getQuestions() {
-    }
-
-    @Test
-    void getMyQuestions() {
-    }
-
-    @Test
-    void deleteQuestion() {
-    }
-
-    @Test
-    void postQuestionForGuest() {
-    }
-
-    @Test
-    void getGuestQuestions() {
-    }
-}
+//package com.example.dice_talk.question.controller;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class QuestionControllerTest {
+//
+//    @Test
+//    void postQuestion() {
+//    }
+//
+//    @Test
+//    void patchQuestion() {
+//    }
+//
+//    @Test
+//    void getQuestion() {
+//    }
+//
+//    @Test
+//    void getQuestions() {
+//    }
+//
+//    @Test
+//    void getMyQuestions() {
+//    }
+//
+//    @Test
+//    void deleteQuestion() {
+//    }
+//
+//    @Test
+//    void postQuestionForGuest() {
+//    }
+//
+//    @Test
+//    void getGuestQuestions() {
+//    }
+//}
